04d7f648cc97e5f24544ecd7c621a539136aa47f
Senfire <eric.mcghee@northwestern.edu>
2022-07-06 23:57:02 -0500

    Added the start button and tested with console log - passed

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+//Adding the Quiz start button for testing
+var startQuizButton = document.querySelector(".Start-quiz");
+
+
+
+startQuizButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log('It worked');
+});
